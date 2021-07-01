fb6afb2c2387a9e1d6dbfdcb4a76da2c69f19097
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-01 07:29:59 +0530

    Added Gender and Date in Employee

--- EmpPayrollData.js
+++ EmpPayrollData.js
@@ -6,10 +6,12 @@ class EmpPayrollData {
 
 
 //Constructor
-constructor(id, name, salary) {
-    this.id = id;
-    this.name = name;
-    this.salary = salary;
+constructor(...params) {
+    this.id = params[0];
+    this.name = params[1];
+    this.salary = params[2];
+    this.gender = params[3];
+    this.startDate = params[4];
 }
 
     get name() { return this._name };
@@ -18,11 +20,18 @@ constructor(id, name, salary) {
     }
 
     toString() {
-        return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary;
+        let options = {year: 'numeric', month: 'long', day: 'numeric'}
+        let empDate = !this.startDate ? "undefined" :
+                        this.startDate.toLocaleDateString("en-us", options)
+        return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary +
+                " gender=" + this.gender + ", startDate= " + empDate;
     }
 }
 
 let empPayrollData = new EmpPayrollData(1, "Mark", 300000);
 console.log(empPayrollData.toString());
 empPayrollData.name = "John";
+console.log(empPayrollData.toString());
+
+empPayrollData = new EmpPayrollData(2, "Terissa", 400000, "F", new Date());
 console.log(empPayrollData.toString());
