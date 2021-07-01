628f78f10509b159ead6f77ae337301b885f0216
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-01 09:44:44 +0530

    Added Validation for gender, salary, id and startDate

--- EmpPayrollData.js
+++ EmpPayrollData.js
@@ -1,20 +1,24 @@
 class EmpPayrollData {
-    id;
-    salary;
-    gender;
-    startDate;
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
+    get id() { return this._id }
+    set id(id) {
+        if (id == undefined)
+            throw 'Assign Value to id';
 
+        if (id <= 0)
+            throw 'Id should be non Zero and Positive!';
+        else {
+            this._id = id;
+        }
+    }
 
-//Constructor
-constructor(...params) {
-    this.id = params[0];
-    this.name = params[1];
-    this.salary = params[2];
-    this.gender = params[3];
-    this.startDate = params[4];
-}
-
-    get name() { return this._name };
+    get name() { return this._name }
     set name(name) {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
         if (nameRegex.test(name))
@@ -24,16 +28,54 @@ constructor(...params) {
         }
     }
 
+    get gender() { return this._gender }
+    set gender(gender) {
+        if (gender == undefined) {
+            throw 'Assign Value to gender';
+        }
+
+        if (gender === "M" || gender == "F")
+            this._gender = gender;
+        else {
+            throw 'Gender enter is incorrect, You have to enter M or F';
+        }
+    }
+
+    get salary() { return this._salary };
+    set salary(salary) {
+        if (salary == undefined) {
+            throw 'Assign Value to salary';
+        }
+
+        if (salary <= 0)
+            throw 'Salary should be non Zero and Positive!';
+        else {
+            this._salary = salary;
+        }
+    }
+
+    get startDate() { return this._startDate }
+    set startDate(startDate) {
+        if (startDate == undefined) {
+        throw 'Assign Value to startDate';
+        }
+        
+        let now = new Date();
+        if(startDate > now) throw 'Start Date should not be future Date';
+        this._startDate = startDate;
+    }
+    
+    
     toString() {
         let options = {year: 'numeric', month: 'long', day: 'numeric'}
         let empDate = !this.startDate ? "undefined" :
                         this.startDate.toLocaleDateString("en-us", options)
         return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary +
-                " gender=" + this.gender + ", startDate= " + empDate;
+                ", gender=" + this.gender + ", startDate= " + empDate;
     }
 }
 
-let empPayrollData = new EmpPayrollData(1, "Mark", 300000);
+let empPayrollData = new EmpPayrollData(1, "Mark", 300000, "M", new Date());
 console.log(empPayrollData.toString());
 try {
     empPayrollData.name = "john";
@@ -41,5 +83,35 @@ try {
 } catch(e) {
     console.error(e);
 }
+
+try {
+    empPayrollData.gender = "d";
+    console.log(empPayrollData.toString());
+} catch(e) {
+    console.error(e);
+}
+
+try {
+    empPayrollData.id = "0";
+    console.log(empPayrollData.toString());
+} catch(e) {
+    console.error(e);
+}
+
+try {
+    empPayrollData.salary = "0";
+    console.log(empPayrollData.toString());
+} catch(e) {
+    console.error(e);
+}
+
+try {
+    let date = "2021-07-02"
+    empPayrollData.startDate = new Date(date);
+    console.log(empPayrollData.toString());
+} catch(e) {
+    console.error(e);
+}
+
 empPayrollData = new EmpPayrollData(2, "Terissa", 400000, "F", new Date());
 console.log(empPayrollData.toString());
